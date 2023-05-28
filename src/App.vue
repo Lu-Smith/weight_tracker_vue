fa2e8de85cb0b02951c9284f38cc1f99dd507deb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-28 17:42:26 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script >
-
-</script>
-
 <template>
 <h1>hello world</h1>
+<input type="number" placeholder="enter your weight in kg..." v-model="weight">
+<p>{{ weight }} kg</p>
 </template>
+
+<script setup >
+  import { ref} from 'vue'
+
+  // const weights = ref([])
+  const weight = ref(null)
+  
+</script>
 
 <styles>
 
