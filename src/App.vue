294df8e983e59c81c339dd7210dd010694964d88
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-29 23:41:05 +0100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
 <h1>Weight Tracker</h1>
 <form @submit.prevent="addWeight">
-<input type="number" placeholder="enter your weight..." v-model="currentWeight" step="0.1"><span>kg</span>
+<input type="number" placeholder="enter your weight..." v-model="weightInput" step="0.1"><span>kg</span>
 <input type="submit" value="Add weight">
 </form>
 <p>{{ currentWeight }} kg</p>
 </template>
 
 <script setup >
-  import { ref} from 'vue'
+  import { computed, ref} from 'vue'
 
   const weights = ref([])
-  const currentWeight = ref(null)
+  const currentWeight = computed(() => {
+    return weights.value.sort((a, b) => b.date - a.date)[0] || 0
+  })
+  const weightInput = ref(60.0)
+  
 
   const addWeight = () => {
     weights.value.push({
-      weight: currentWeight.value,
+      weight: weightInput.value,
       date: new Date().getTime()
     })
   }
