ce904bb77e9e45d78ed1ac596fb009fc43cfc75f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-05-31 10:46:31 +0100

    Updated styles in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,4 +3,45 @@
     <a href="https://github.com/Lu-Smith/weight_tracker_vue" target="_blank">GitHub</a>.</footer>
   </template>
   
+  <style>
+  footer {
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      left: 0;
+      text-align: center;
+      background-color: #f2f2f2;
+      padding: 20px;
   
+  }
+  
+  footer a {
+    color: #4f0779;
+    text-decoration: none;
+    position: relative;
+    transition: color 0.3s;
+  }
+  
+  footer a::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    bottom: -5px;
+    left: 0;
+    background-color: #e69823;;
+    visibility: hidden;
+    transform: scaleX(0);
+    transition: all 0.3s ease-in-out;
+  }
+  
+  footer a:hover {
+    color: #e69823;
+  }
+  
+  footer a:hover::before {
+    visibility: visible;
+    transform: scaleX(1);
+  }
+
+  </style>
